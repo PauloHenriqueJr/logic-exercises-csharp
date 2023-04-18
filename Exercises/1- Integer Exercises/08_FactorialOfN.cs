d43bf5ebace905efedef8385889cffa40f64a577
Paulo Henrique De Jesus <paulo.jesus@stone.com.br>
2023-04-17 21:41:12 -0300

    feat: add new exercise FactorialOfN

---
+++ Exercises/1- Integer Exercises/08_FactorialOfN.cs
@@ -0,0 +1,21 @@
+﻿namespace Exercises
+{
+    public class FactorialOfN
+    {
+        public static void Solution1()
+        {
+            Console.WriteLine("\nCálculo do fatorial de um número inteiro.\n");
+            Console.Write("Digite um inteiro nao-negativo: ");
+
+            int n = int.Parse(Console.ReadLine());
+            int factorial = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine($"\nO fatorial de {n} é {factorial}.\n");
+        }
+    }
+}
