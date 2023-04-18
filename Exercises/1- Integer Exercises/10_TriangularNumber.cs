8ff56fa4e6a6456adc5983d3ed80cde7d225c220
Paulo Henrique De Jesus <paulo.jesus@stone.com.br>
2023-04-18 16:37:31 -0300

    feat: add new exercise TriangularNumber

---
+++ Exercises/1- Integer Exercises/10_TriangularNumber.cs
@@ -0,0 +1,30 @@
+﻿namespace Exercises
+{
+    public class TriangularNumber
+    {
+        public static void Solution1()
+        {
+            Console.WriteLine("Cálculo do número triangular de um número inteiro.");
+
+            Console.Write("Digite um número inteiro não-negativo: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int i = 0;
+
+            while (i * (i + 1) * (i + 2) < n)
+            {
+                i++;
+            }
+
+            if (i * (i + 1) * (i + 2) == n)
+            {
+                Console.WriteLine($"{n} é um número triangular, pois é o produto de {i} x {i + 1} x {i + 2}.");
+            }
+            else
+            {
+                Console.WriteLine($"{n} não é um número triangular.");
+            }
+        }
+
+    }
+}
