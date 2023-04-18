9dc26e957d410daf76094c01cf5eaea88b3cba29
Paulo Henrique De Jesus <paulo.jesus@stone.com.br>
2023-04-18 16:37:05 -0300

    feat: add new exercise MultiplesOfNumbers

---
+++ Exercises/1- Integer Exercises/09_MultiplesOfNumbers.cs
@@ -0,0 +1,30 @@
+﻿namespace Exercises
+{
+    public class MultiplesOfNumbers
+    {
+        public static void Solution1()
+        {
+            Console.WriteLine("\nCálculo dos múltiplos de um número.\n");
+
+            Console.Write("Digite um número inteiro: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite um número inteiro: ");
+            int i = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite um número inteiro: ");
+            int j = int.Parse(Console.ReadLine());
+
+            int multi = 0;
+
+            for (int count = 1; multi <= n; count++)
+            {
+                if (count % i == 0 || count % j == 0)
+                {
+                    Console.WriteLine(count);
+                    multi++;
+                }
+            }
+        }
+    }
+}
