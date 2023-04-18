0c76c39adcee373d8ecc02737b40711978b239ef
Paulo Henrique De Jesus <paulo.jesus@stone.com.br>
2023-04-17 21:39:30 -0300

    feat: add new exercise SumNumbersPairs

---
+++ Exercises/1- Integer Exercises/07_SumNumbersPairs.cs
@@ -0,0 +1,28 @@
+﻿namespace Exercises
+{
+    public class SumNumbersPairs
+    {
+        public static void Solution1()
+        {
+            Console.WriteLine("\nSoma de números pares.\n");
+
+            Console.Write("Digite a quantidade de números: ");
+            int n = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("Digite o número " + (i + 1) + ": ");
+                int num = int.Parse(Console.ReadLine());
+
+                if (num % 2 == 0)
+                {
+                    sum += num;
+                }
+            }
+
+            Console.WriteLine($"\nA soma dos números pares é {sum}.\n");
+        }
+    }
+}
