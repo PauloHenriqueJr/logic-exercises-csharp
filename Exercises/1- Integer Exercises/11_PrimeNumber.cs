5df87d0380e580d52c60d05629161df4784d4a86
Paulo Henrique De Jesus <paulo.jesus@stone.com.br>
2023-04-18 16:37:47 -0300

    feat: add new exercise PrimeNumber

---
+++ Exercises/1- Integer Exercises/11_PrimeNumber.cs
@@ -0,0 +1,28 @@
+﻿namespace Exercises
+{
+    public class PrimeNumber
+    {
+        public static void Solution1()
+        {
+            Console.WriteLine("Cálculo de números primos.");
+            Console.Write("Digite um número inteiro não-negativo: ");
+
+            int n = int.Parse(Console.ReadLine());
+
+            bool isPrime = true;
+
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+
+            string message = isPrime ? $"{n} é um número primo." : $"{n} não é um número primo.";
+            Console.WriteLine(message);
+
+        }
+    }
+}
