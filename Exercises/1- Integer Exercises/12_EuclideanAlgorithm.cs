1b681634e785843b43386e1e4eba881de82d12df
Paulo Henrique De Jesus <paulo.jesus@stone.com.br>
2023-04-24 10:26:40 -0300

    feat: add new exercise EuclideanAlgorithm

---
+++ Exercises/1- Integer Exercises/12_EuclideanAlgorithm.cs
@@ -0,0 +1,28 @@
+﻿namespace Exercises
+{
+    public class EuclideanAlgorithm
+    {
+        public static void Solution1()
+        {
+            Console.WriteLine("Cálculo do MDC de dois números inteiros.");
+
+            Console.Write("Digite um número inteiro: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite um número inteiro: ");
+            int b = int.Parse(Console.ReadLine());
+
+            int mdc = 0;
+
+            for (int i = 1; i <= a && i <= b; i++)
+            {
+                if (a % i == 0 && b % i == 0)
+                {
+                    mdc = i;
+                }
+            }
+
+            Console.WriteLine($"O MDC de {a} e {b} é {mdc}.");
+        }
+    }
+}
